97abf6353777226b879e34d665cc38984eb01921
ran <abbshrsoufii@gmail.com>
2019-12-11 18:05:15 +0800

    refactor: 使用 future 实现

--- src/main.rs
+++ src/main.rs
@@ -1,159 +1,113 @@
-// // 创建包含1-10，十个数字的数组，用两个线程依次从数组中获取元素，每100ms 获取一个。这一行为封装成一个 Future， 拿到五个后完成Future。
-// // 主线程在两个子线程Future结束后退出。
+#![allow(unused)]
 
-// #![allow(unused)]
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::{Arc, Mutex},
+    sync::mpsc::{channel, Sender, Receiver},
+    task::{Context, Waker, Poll},
+    thread,
+    time::Duration,
+    io,
+};
 
-// use std::thread;
-// use std::sync::mpsc::channel;
-// use std::sync::mpsc::Sender;
-// use std::time::Duration;
+use futures::executor;
+use futures::future::join;
 
-// fn main() {
-//     let vec: Vec<u16> = (1..=10).collect();
-
-//     let mut chans: [Option<Sender<Option<u16>>>; 2] = [None, None];
-//     let mut thrs: [Option<thread::JoinHandle<()>>; 2] = [None, None];
-
-//     for i in 0..2 {
-//         let (sender, receiver) = channel();
-//         chans[i] = Some(sender);
-
-//         // 工作线程负责接收任务
-//         let thr = thread::spawn(move || {
-//             let tid = thread::current().id();
-
-//             // TODO: use Future
-//             loop {
-//                 match receiver.recv() {
-//                     Ok(option) => {
-//                         match option {
-//                             Some(elem) => println!("{:?} consume element: {:?}", tid, elem),
-//                             None => {
-//                                 println!("{:?} consume done", tid);
-//                                 break;
-//                             },
-//                         }
-//                     },
-//                     Err(err) => println!("recv err: {:?}", err),
-//                 }
-//             }
-
-//             println!("{:?} terminate", tid);
-//         });
-
-//         thrs[i] = Some(thr);
-//     }
-
-//     // 主线程负责调度策略
-//     for i in 0..vec.len() {
-//         thread::sleep(Duration::from_millis(100));
-//         if let Some(ref chan) = &chans[i % &chans.len()] {
-//             &chan.send(Some(vec[i]));
-//         }
-//     }
-
-//     // 依次通知结束
-//     for i in 0..chans.len() {
-//         if let Some(ref chan) = &chans[i] {
-//             &chan.send(None);
-//         }
-//     }
-
-//     // 等待所有工作线程退出
-//     for i in 0..thrs.len() {
-//         thrs[i].take().unwrap().join();
-//     }
-// }
-
-extern crate futures;
-
-use std::sync::mpsc::{Sender, Receiver, channel, TryRecvError};
-use futures::{Poll, executor};
-use futures::task::{Context, Waker};
-use core::pin::Pin;
-use std::thread;
-use std::time::Duration;
-
-struct Consumer<'a> {
-    sender: Option<Sender<Option<u16>>>;
-    thread_handler: Option<thread::JoinHandle<()>>;
-    waker: Option<&'a Waker>;
-    finished: bool;
+pub struct ConsumerFuture {
+    shared_state: Arc<Mutex<SharedState>>,
 }
 
-impl Future for Consumer<'a> {
+struct SharedState {
+    completed: bool,
+    waker: Option<Waker>,
+}
+
+impl Future for ConsumerFuture {
     type Output = ();
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-        if self.finished {
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut shared_state = self.shared_state.lock().unwrap();
+        if shared_state.completed {
             Poll::Ready(())
         } else {
-            match self.waker {
-                None => self.waker = Some(cx.waker()),
-                _ => (),
-            }
+            shared_state.waker = Some(cx.waker().clone());
             Poll::Pending
         }
     }
 }
 
-impl Consumer<'a> {
-    fn new() -> Consumer<'a> {
-        let consumer = Consumer {
-            sender: None;
-            thread_handler: None;
-            waker: None;
-            finished: false;
-        };
+impl ConsumerFuture {
+    pub fn new(receiver: Receiver<Option<u16>>) -> Self {
+        let shared_state = Arc::new(Mutex::new(SharedState {
+            completed: false,
+            waker: None,
+        }));
 
-        let (sender, receiver) = channel();
+        let thread_shared_state = shared_state.clone();
 
-        consumer.sender = sender;
-
-        let thread_handler = thread::spawn(move || {
+        thread::spawn(move || {
             let tid = thread::current().id();
-
             loop {
                 match receiver.recv() {
                     Ok(option) => {
                         match option {
                             Some(elem) => println!("{:?} consume element: {:?}", tid, elem),
                             None => {
-                                println!("{:?} consume done", tid);
-                                consumer.finished = true;
-                                if let Some(waker) = consumer.waker {
-                                    waker.wake();
+                                println!("future <{:?}> state change -> completed", tid);
+                                let mut shared_state = thread_shared_state.lock().unwrap();
+                                shared_state.completed = true;
+                                if let Some(waker) = shared_state.waker.take() {
+                                    waker.wake()
                                 }
                                 break;
                             },
                         }
                     },
-                    Err(err) => {
-                        println!("recv err: {:?}", err);
-                        consumer.finished = true;
-                        if let Some(waker) = consumer.waker {
-                            waker.wake();
-                        }
-                        break;
-                    },
+                    Err(err) => println!("{:?} recv err: {:?}", tid, err),
                 }
             }
-
-            println!("{:?} terminate", tid);
         });
 
-        consumer.thread_handler = thread_handler;
-        consumer
+        ConsumerFuture {
+            shared_state,
+        }
     }
 }
 
-async waitAll(consumer_1: Consumer<'a>, consumer_2: Consumer<'a>) {
-    join!(consumer_1, consumer_2)
-}
-
 fn main() {
-    let consumer_1 = Consumer::new();
-    let consumer_2 = Consumer::new();
+    let vec: Vec<u16> = (1..=10).collect();
 
-    executor.block_on(waitAll(consumer_1, consumer_2));
+    let (sender_1, receiver_1) = channel();
+    let (sender_2, receiver_2) = channel();
+
+    let chans = [
+        &sender_1,
+        &sender_2,
+    ];
+
+    let consumer_future_1 = ConsumerFuture::new(receiver_1);
+    let consumer_future_2 = ConsumerFuture::new(receiver_2);
+
+    let waiter = thread::spawn(move || {
+        let handler = join(consumer_future_1, consumer_future_2);
+        // 阻塞, 等待所有 future 结束
+        executor::block_on(handler);
+
+        println!("all future (threads) done");
+    });
+
+    // 主线程负责协调工作线程实现要求的调度策略
+    for i in 0..vec.len() {
+        thread::sleep(Duration::from_millis(100));
+        let chan = &chans[i % &chans.len()];
+        chan.send(Some(vec[i]));
+    }
+
+    for i in 0..chans.len() {
+        let chan = &chans[i];
+        chan.send(None);
+    }
+
+    waiter.join();
 }
